3d240b00b2b789be197e59cdcd8faaa12dcc35d6
you2095 <iapoc.hsk@gmail.com>
2020-01-22 14:10:36 +0900

    chat-space-自動更新機能 [修正]

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,7 +84,7 @@ $(function(){
     }
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
